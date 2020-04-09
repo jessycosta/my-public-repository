706b359b454abb976818dcff782fb6d0380f87da
jessycosta <63069671+jessycosta@users.noreply.github.com>
2020-04-09 21:49:45 +0100

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,10 +1,10 @@
 # Fibonacci numbers module
 
 def fib(n):   # write Fibonacci series up to n
-    a, b = 0, 1
-    while b < n:
-        print(b, end=' ')
-        a, b = b, a+b
+    e, f = 0, 1
+    while f < n:
+        print(f, end=' ')
+        e, f = f, e+f
     print()
 
 def fib2(n):   # return Fibonacci series up to n
